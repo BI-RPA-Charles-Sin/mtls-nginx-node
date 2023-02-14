68acd0fb519a15d38c28990551d4474b75713ee5
Charles_Sin <charles_sin@mail.chungyo.net>
2023-02-14 13:37:36 +0800

    first commit

---
+++ node-server/server.js
@@ -0,0 +1,63 @@
+const express = require("express");
+const https = require("https");
+const fs = require("fs");
+const path = require("path");
+const forge = require("node-forge");
+
+const port = 3000;
+
+const options = {
+  ca: fs.readFileSync(path.resolve(__dirname, "./certs/ca.crt")),
+  cert: fs.readFileSync(path.resolve(__dirname, "./certs/server.crt")),
+  key: fs.readFileSync(path.resolve(__dirname, "./certs/server.key")),
+  rejectUnauthorized: false,
+  requestCert: true,
+};
+
+const app = express();
+
+app.get("/", (req, res) => {
+  if (!isEmpty(req.socket.getPeerCertificate())) {
+    return verify_certificate(req, res);
+  }
+
+  if (req.header("ssl_client_verify") !== "SUCCESS") return res.status(403).send("Forbidden - please provide valid certificate.");
+
+  const sslCert = req.header("X-SSL-CERT");
+  const cert = decodeURIComponent(sslCert);
+  const forgeCert = forge.pki.certificateFromPem(cert);
+
+  console.log(forgeCert);
+
+  res.status(200).json(`Hello ${req.header("ssl_client")}, your certificate was issued by ${req.header("SSL_Client_Issuer")}!`);
+});
+
+https.createServer(options, app).listen(port, () => {
+  console.log(`.. server up and running and listening on ${port} ..`);
+});
+
+function verify_certificate(request, response) {
+  const cert = request.socket.getPeerCertificate();
+
+  console.log("request.socket.getPeerCertificate() value : ");
+  console.log(request.socket.getPeerCertificate());
+
+  if (request.client.authorized) {
+    return response.send(`Hello ${cert.subject.CN}, your certificate was issued by ${cert.issuer.CN}!`);
+  }
+
+  if (cert.subject) {
+    return response.status(403).send(`Sorry ${cert.subject.CN}, certificates from ${cert.issuer.CN} are not welcome here.`);
+  } else {
+    return response.status(401).send(`Sorry, but you need to provide a client certificate to continue.`);
+  }
+}
+
+function isEmpty(obj) {
+  for (var key in obj) {
+    if (obj.hasOwnProperty(key)) return false;
+  }
+  return true;
+}
+
+// '63:77:21:B2:25:0D:B6:64:41:9C:72:A2:7D:3E:48:09:B1:72:CC:F6',
