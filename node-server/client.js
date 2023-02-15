da6e6e1e3864e10e5862c4aa26acffaf30c63225
Charles_Sin <charles_sin@mail.chungyo.net>
2023-02-15 15:45:01 +0800

    update

--- node-server/client.js
+++ node-server/client.js
@@ -37,7 +37,7 @@ const getRequestWithCertificate = async () => {
         // the request was made and server responsed tiwh a status code
         // console.log(error.response.data);
         // console.log(error.response.status);
-        // console.log(error.response.headers); 
+        // console.log(error.response.headers);
       }
     }
   }
@@ -46,76 +46,3 @@ const getRequestWithCertificate = async () => {
 setTimeout(() => {
   getRequestWithCertificate();
 }, 10);
-
-/**
- * {
-  "sni": "kawayiyi.com",
-  "tlsVersion": "Tls13",
-  "tcpConnectionId": "0HML9VNSSHQ9M",
-  "tlsHashOrigin": "771,4865-4866-4867-49195-49199-49196-49200-52393-52392-49171-49172-156-157-47-53,5-10-16-13-65281-11-18-45-51-35-0-23-27-17513-43-41,29-23-24,0",
-  "tlsHashMd5": "b5d0127ecf5592c8ac069a74fa413a1a",
-  
-  "cipherList": [
-    "TLS_AES_128_GCM_SHA256",
-    "TLS_AES_256_GCM_SHA384",
-    "TLS_CHACHA20_POLY1305_SHA256",
-    "TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256",
-    "TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256",
-    "TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384",
-    "TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384",
-    "TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256",
-    "TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305_SHA256",
-    "TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA",
-    "TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA",
-    "TLS_RSA_WITH_AES_128_GCM_SHA256",
-    "TLS_RSA_WITH_AES_256_GCM_SHA384",
-    "TLS_RSA_WITH_AES_128_CBC_SHA",
-    "TLS_RSA_WITH_AES_256_CBC_SHA"
-  ],
-  "extentions": [
-    "status_request",
-    "supported_groups",
-    "application_layer_protocol_negotiation",
-    "signature_algorithms",
-    "renegotiation_info",
-    "ec_point_formats",
-    "signed_certificate_timestamp",
-    "psk_key_exchange_modes",
-    "key_share",
-    "session_ticket",
-    "server_name",
-    "extended_master_secret",
-    "compress_certificate",
-    "extensionApplicationSettings",
-    "supported_versions",
-    "pre_shared_key"
-  ],
-  "supportedgroups": [
-    "X25519",
-    "CurveP256",
-    "CurveP384"
-  ],
-  "ecPointFormats": [
-    "uncompressed"
-  ],
-  "proto": "HTTP/2",
-  "h2": {
-    "SETTINGS": {
-      "1": "65536",
-      "2": "0",
-      "3": "1000",
-      "4": "6291456",
-      "6": "262144"
-    },
-    "WINDOW_UPDATE": "15663105",
-    "HEADERS": [
-      ":method",
-      ":authority",
-      ":scheme",
-      ":path"
-    ]
-  },
-  "user_agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36",
-  "clientIp": "165.84.247.15"
-}
- */
--- node-server/server.js
+++ node-server/server.js
@@ -29,13 +29,14 @@ app.get("/", (req, res) => {
 
   console.log("forgeCert.generateSubjectKeyIdentifier() : ");
   console.log(forgeCert.generateSubjectKeyIdentifier());
+
   // Unique
-  // console.log("ssl_curves : unique");
-  // console.log("ssl_curves : ");
-  // console.log(req.header("ssl_curves"));
+  console.log("ssl_curves : unique");
+  console.log("ssl_curves : ");
+  console.log(req.header("ssl_curves"));
   // Change every time
-  // console.log("ssl_session_id : ");
-  // console.log(req.header("ssl_session_id"));
+  console.log("ssl_session_id : ");
+  console.log(req.header("ssl_session_id"));
 
   // CUSTOM PRINT OUT WHAT IS THAT.
   // console.log("X-Forwarded-For");
@@ -62,9 +63,9 @@ function verify_certificate(request, response) {
   const cert = request.socket.getPeerCertificate();
 
   console.log("request.socket value : ");
-  console.log(request.socket.getCipher());
-  console.log(request.socket.getSession());
-  console.log(request.socket.getTLSTicket());
+  // console.log(request.socket.getCipher());
+  // console.log(request.socket.getSession());
+  // console.log(request.socket.getTLSTicket());
 
   if (request.client.authorized) {
     return response.send(`Hello ${cert.subject.CN}, your certificate was issued by ${cert.issuer.CN}!`);
@@ -83,76 +84,3 @@ function isEmpty(obj) {
   }
   return true;
 }
-
-/**
- * {
-  "sni": "kawayiyi.com",
-  "tlsVersion": "Tls13",
-  "tcpConnectionId": "0HML9VNSSHQ9L",
-  "tlsHashOrigin": "771,4865-4866-4867-49195-49199-49196-49200-52393-52392-49171-49172-156-157-47-53,45-43-27-16-17513-18-35-10-11-51-13-65281-0-5-23-41,29-23-24,0",
-  "tlsHashMd5": "73df07b3a44d59d95e7af9a16988ff89",
-  
-  "cipherList": [
-    "TLS_AES_128_GCM_SHA256",
-    "TLS_AES_256_GCM_SHA384",
-    "TLS_CHACHA20_POLY1305_SHA256",
-    "TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256",
-    "TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256",
-    "TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384",
-    "TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384",
-    "TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256",
-    "TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305_SHA256",
-    "TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA",
-    "TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA",
-    "TLS_RSA_WITH_AES_128_GCM_SHA256",
-    "TLS_RSA_WITH_AES_256_GCM_SHA384",
-    "TLS_RSA_WITH_AES_128_CBC_SHA",
-    "TLS_RSA_WITH_AES_256_CBC_SHA"
-  ],
-  "extentions": [
-    "psk_key_exchange_modes",
-    "supported_versions",
-    "compress_certificate",
-    "application_layer_protocol_negotiation",
-    "extensionApplicationSettings",
-    "signed_certificate_timestamp",
-    "session_ticket",
-    "supported_groups",
-    "ec_point_formats",
-    "key_share",
-    "signature_algorithms",
-    "renegotiation_info",
-    "server_name",
-    "status_request",
-    "extended_master_secret",
-    "pre_shared_key"
-  ],
-  "supportedgroups": [
-    "X25519",
-    "CurveP256",
-    "CurveP384"
-  ],
-  "ecPointFormats": [
-    "uncompressed"
-  ],
-  "proto": "HTTP/2",
-  "h2": {
-    "SETTINGS": {
-      "1": "65536",
-      "2": "0",
-      "3": "1000",
-      "4": "6291456",
-      "6": "262144"
-    },
-    "WINDOW_UPDATE": "15663105",
-    "HEADERS": [
-      ":method",
-      ":authority",
-      ":scheme",
-      ":path"
-    ]
-  },
-  "user_agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36",
-  "clientIp": "165.84.247.15"
-}
- */
