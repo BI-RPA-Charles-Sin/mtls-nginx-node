d693edf143aa44ef0d0ab481d63d8e814a380ea2
Charles_Sin <charles_sin@mail.chungyo.net>
2023-02-22 08:52:40 +0800

    retrive to old version.

--- node-server/server.js
+++ node-server/server.js
@@ -7,7 +7,6 @@ const md5 = require("md5");
 const cors = require("cors");
 
 const PORT = 3000;
-const HOSTNAME = "34.92.220.98";
 
 const options = {
   ca: fs.readFileSync(path.resolve(__dirname, "./certs/ca.crt")),
@@ -86,7 +85,7 @@ app.get("/api/cert", (req, res) => {
   // res.sendFile("./index.html", { root: __dirname });
 });
 
-https.createServer(options, app).listen({ PORT, HOSTNAME }, () => {
+https.createServer(options, app).listen(PORT, () => {
   console.log(`.. server up and running and listening on ${PORT} ..`);
 });
 
