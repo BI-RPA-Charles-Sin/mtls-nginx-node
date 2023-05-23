8b5490eb228917bfbc9edf60ff03f37e8a16ce12
Charles_Sin <charles_sin@mail.chungyo.net>
2023-05-23 09:40:22 +0800

    1. comment unuse code.

--- node-server/server.js
+++ node-server/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const https = require("https");
+const http = require("http");
 const fs = require("fs");
 const path = require("path");
 const forge = require("node-forge");
@@ -19,7 +20,8 @@ const options = {
 
 const app = express();
 
-const server = https.createServer(options, app);
+// const server = https.createServer(options, app);
+const server = http.createServer(options, app);
 server.listen(PORT, () => {
   console.log(`.. server up and running and listening on ${PORT} ..`);
 });
@@ -28,74 +30,74 @@ app.use(cors());
 app.use(express.static(path.join(__dirname, "public")));
 
 // Socket Begin
-const io = require("socket.io")();
+// const io = require("socket.io")();
 
-var servIo = io.listen(server, {
-  cors: true,
-  origin: "*",
-  credentials: true,
-  forceBase64: true,
-  path: "/api/cert",
-});
+// const servIo = io.listen(server, {
+//   cors: true,
+//   origin: "*",
+//   credentials: true,
+//   forceBase64: true,
+//   path: "/api/cert",
+// });
 
-servIo.on("connection", function (socket) {
-  setInterval(function () {
-    const tls_version = socket.handshake.headers["x-https-protocol"];
-    const clientIp = socket.handshake.headers["x-real-ip"];
-    const ellipticCurvesArr = socket.handshake.headers["ssl_curves"].split(":");
-    const ciphersArr = socket.handshake.headers["ssl_ciphers"].split(":");
-    const clientFp = socket.handshake.auth;
+// servIo.on("connection", function (socket) {
+//   setInterval(function () {
+//     const tls_version = socket.handshake.headers["x-https-protocol"];
+//     const clientIp = socket.handshake.headers["x-real-ip"];
+//     const ellipticCurvesArr = socket.handshake.headers["ssl_curves"].split(":");
+//     const ciphersArr = socket.handshake.headers["ssl_ciphers"].split(":");
+//     const clientFp = socket.handshake.auth;
 
-    let ellipticCurves = "";
-    let ciphers = "";
+//     let ellipticCurves = "";
+//     let ciphers = "";
 
-    ellipticCurvesArr.forEach((item) => {
-      ellipticCurves += `${item}-`;
-    });
+//     ellipticCurvesArr.forEach((item) => {
+//       ellipticCurves += `${item}-`;
+//     });
 
-    ciphersArr.forEach((item) => {
-      ciphers += `${item}-`;
-    });
+//     ciphersArr.forEach((item) => {
+//       ciphers += `${item}-`;
+//     });
 
-    ellipticCurves = ellipticCurves.slice(0, -1) + "";
-    ciphers = ciphers.slice(0, -1) + "";
+//     ellipticCurves = ellipticCurves.slice(0, -1) + "";
+//     ciphers = ciphers.slice(0, -1) + "";
 
-    const ja3_str = `${tls_version},${ellipticCurves},${ciphers}`;
-    const ja3Hash = md5(ja3_str);
+//     const ja3_str = `${tls_version},${ellipticCurves},${ciphers}`;
+//     const ja3Hash = md5(ja3_str);
 
-    const token = jwt.sign(
-      {
-        data: { tls_version, ellipticCurves, ciphers, ja3Hash, clientFp, clientIp },
-      },
-      "secret"
-    );
+//     const token = jwt.sign(
+//       {
+//         data: { tls_version, ellipticCurves, ciphers, ja3Hash, clientFp, clientIp },
+//       },
+//       "secret"
+//     );
 
-    fs.readFile("myjsonfile.json", "utf8", function readFileCallback(err, data) {
-      if (err) {
-        console.log(err);
-      } else {
-        let obj = JSON.parse(data); //now it an object
+//     fs.readFile("myjsonfile.json", "utf8", function readFileCallback(err, data) {
+//       if (err) {
+//         console.log(err);
+//       } else {
+//         let obj = JSON.parse(data); //now it an object
 
-        obj.table.push({ tls_version, ellipticCurves, ciphers, ja3Hash, clientFp, clientIp }); //add some data
+//         obj.table.push({ tls_version, ellipticCurves, ciphers, ja3Hash, clientFp, clientIp }); //add some data
 
-        const json = JSON.stringify(obj); //convert it back to json
+//         const json = JSON.stringify(obj); //convert it back to json
 
-        fs.writeFile("myjsonfile.json", json, "utf8", (err) => {
-          // error checking
-          if (err) throw err;
+//         fs.writeFile("myjsonfile.json", json, "utf8", (err) => {
+//           // error checking
+//           if (err) throw err;
 
-          // console.log("New data added");
-        }); // write it back
-      }
-    });
+//           // console.log("New data added");
+//         }); // write it back
+//       }
+//     });
 
-    socket.handshake.jwt = token;
+//     socket.handshake.jwt = token;
 
-    socket.emit("second", { second: new Date().getTime(), ja3Hash });
-  }, 1000);
+//     socket.emit("second", { second: new Date().getTime(), ja3Hash });
+//   }, 1000);
 
-  console.log("connect");
-});
+//   console.log("connect");
+// });
 // Socket End
 
 app.get("/", (req, res) => {
@@ -108,86 +110,86 @@ app.get("/read", (req, res) => {
   res.status(201).json(myObject);
 });
 
-app.get("/api/cert", (req, res) => {
-  console.log("api/certs");
-  if (!isEmpty(req.socket.getPeerCertificate())) {
-    return verify_certificate(req, res);
-  }
+// app.get("/api/cert", (req, res) => {
+//   console.log("api/certs");
+//   if (!isEmpty(req.socket.getPeerCertificate())) {
+//     return verify_certificate(req, res);
+//   }
 
-  // if (req.header("ssl_client_verify") !== "SUCCESS") return res.status(403).send("Forbidden - please provide valid certificate.");
+//   // if (req.header("ssl_client_verify") !== "SUCCESS") return res.status(403).send("Forbidden - please provide valid certificate.");
 
-  let tls_version = req.header("X-HTTPS-Protocol");
-  let ellipticCurvesArr = req.header("ssl_curves").split(":");
-  let ciphersArr = req.header("ssl_ciphers").split(":");
+//   let tls_version = req.header("X-HTTPS-Protocol");
+//   let ellipticCurvesArr = req.header("ssl_curves").split(":");
+//   let ciphersArr = req.header("ssl_ciphers").split(":");
 
-  let ellipticCurves = "";
-  let ciphers = "";
+//   let ellipticCurves = "";
+//   let ciphers = "";
 
-  ellipticCurvesArr.forEach((item) => {
-    ellipticCurves += `${item}-`;
-  });
+//   ellipticCurvesArr.forEach((item) => {
+//     ellipticCurves += `${item}-`;
+//   });
 
-  ciphersArr.forEach((item) => {
-    ciphers += `${item}-`;
-  });
+//   ciphersArr.forEach((item) => {
+//     ciphers += `${item}-`;
+//   });
 
-  ellipticCurves = ellipticCurves.slice(0, -1) + "";
-  ciphers = ciphers.slice(0, -1) + "";
+//   ellipticCurves = ellipticCurves.slice(0, -1) + "";
+//   ciphers = ciphers.slice(0, -1) + "";
 
-  const ja3_str = `${tls_version},${ellipticCurves},${ciphers}`;
-  const ja3Hash = md5(ja3_str);
+//   const ja3_str = `${tls_version},${ellipticCurves},${ciphers}`;
+//   const ja3Hash = md5(ja3_str);
 
-  // console.log("outside if");
-  // console.log(ja3_str);
-  // console.log(ja3Hash);
+//   // console.log("outside if");
+//   // console.log(ja3_str);
+//   // console.log(ja3Hash);
 
-  // Unique
-  // console.log("ssl_curves : unique");
-  // console.log(req.header("ssl_curves"));
-  // console.log("X-Real-IP");
-  // console.log(req.header("X-Real-IP"));
-  // Change every time
-  // console.log("ssl_session_id : dynamic");
-  // console.log(req.header("ssl_session_id"));
-  // console.log("ssl_cipher");
-  // console.log(req.header("ssl_cipher"));
-  // console.log("ssl_ciphers");
-  // console.log(req.header("ssl_ciphers").split(":"));
-  // console.log("request_id : dynamic");
-  // console.log(req.header("request_id"));
+//   // Unique
+//   // console.log("ssl_curves : unique");
+//   // console.log(req.header("ssl_curves"));
+//   // console.log("X-Real-IP");
+//   // console.log(req.header("X-Real-IP"));
+//   // Change every time
+//   // console.log("ssl_session_id : dynamic");
+//   // console.log(req.header("ssl_session_id"));
+//   // console.log("ssl_cipher");
+//   // console.log(req.header("ssl_cipher"));
+//   // console.log("ssl_ciphers");
+//   // console.log(req.header("ssl_ciphers").split(":"));
+//   // console.log("request_id : dynamic");
+//   // console.log(req.header("request_id"));
 
-  res.status(200).json({
-    msg: `Hello ${req.header("ssl_client")}, your certificate was issued by ${req.header("SSL_Client_Issuer")}! `,
-    tls_version,
-    ellipticCurves,
-    ciphers,
-    ja3_str: `${ja3_str}`,
-    ja3_hash: `${ja3Hash}`,
-  });
-  // res.sendFile("./index.html", { root: __dirname });
-});
+//   res.status(200).json({
+//     msg: `Hello ${req.header("ssl_client")}, your certificate was issued by ${req.header("SSL_Client_Issuer")}! `,
+//     tls_version,
+//     ellipticCurves,
+//     ciphers,
+//     ja3_str: `${ja3_str}`,
+//     ja3_hash: `${ja3Hash}`,
+//   });
+//   // res.sendFile("./index.html", { root: __dirname });
+// });
 
-function verify_certificate(request, response) {
-  const cert = request.socket.getPeerCertificate();
+// function verify_certificate(request, response) {
+//   const cert = request.socket.getPeerCertificate();
 
-  console.log("request.socket value : ");
+//   console.log("request.socket value : ");
 
-  if (request.client.authorized) {
-    return response.send(`Hello ${cert.subject.CN}, your certificate was issued by ${cert.issuer.CN}!`);
-  }
+//   if (request.client.authorized) {
+//     return response.send(`Hello ${cert.subject.CN}, your certificate was issued by ${cert.issuer.CN}!`);
+//   }
 
-  if (cert.subject) {
-    return response.status(403).send(`Sorry ${cert.subject.CN}, certificates from ${cert.issuer.CN} are not welcome here.`);
-  } else {
-    return response.status(401).send(`Sorry, but you need to provide a client certificate to continue.`);
-  }
-}
+//   if (cert.subject) {
+//     return response.status(403).send(`Sorry ${cert.subject.CN}, certificates from ${cert.issuer.CN} are not welcome here.`);
+//   } else {
+//     return response.status(401).send(`Sorry, but you need to provide a client certificate to continue.`);
+//   }
+// }
 
-function isEmpty(obj) {
-  for (var key in obj) {
-    if (obj.hasOwnProperty(key)) return false;
-  }
-  return true;
-}
+// function isEmpty(obj) {
+//   for (var key in obj) {
+//     if (obj.hasOwnProperty(key)) return false;
+//   }
+//   return true;
+// }
 
 // T755TcGHGTiT2UkkAAAA
