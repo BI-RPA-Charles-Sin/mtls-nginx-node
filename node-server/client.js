01c03bdc2c150d5493f8b560b4062e354130a231
Charles_Sin <charles_sin@mail.chungyo.net>
2023-05-26 11:07:05 +0800

    1. update http to https

--- node-server/client.js
+++ node-server/client.js
@@ -7,7 +7,7 @@ const { io } = require("socket.io-client");
 //   path: "/api/sockets",
 // });
 
-const socket = io("http://34.92.128.241", {
+const socket = io("34.92.128.241:8000", {
   path: "/api/sockets",
 });
 
