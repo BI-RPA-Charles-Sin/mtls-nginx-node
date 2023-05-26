336cfa8d97408e23ca02d291b2a0e77adcd2bc8f
Charles_Sin <charles_sin@mail.chungyo.net>
2023-05-26 11:11:23 +0800

    1. update

--- node-server/client.js
+++ node-server/client.js
@@ -7,7 +7,7 @@ const { io } = require("socket.io-client");
 //   path: "/api/sockets",
 // });
 
-const socket = io("34.92.128.241:8000", {
+const socket = io("https://34.92.128.241:8000", {
   path: "/api/sockets",
 });
 
